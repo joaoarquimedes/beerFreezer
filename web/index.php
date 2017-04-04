5d85263c0064ea026cd68db35a2a6e37e8989b4b
Joao Arquimedes de Sousa Costa <joao.costa@enap.gov.br>
2017-04-04 18:34:16 -0300

    Melhoria index.php

--- web/index.php
+++ web/index.php
@@ -1,63 +1,72 @@
 <?php
 $limitPrint = 120;
 $fileJson = "report/beerFreezer.json";
+$alert = null;
 
 # Verifica se o arquivo já foi gerado
 if (! file_exists($fileJson)) {
-  echo "Arquivo JSON ainda não gerado para demonstrar relatório gráfico";
-  exit();
+  $alert = "Arquivo $fileJson ainda não gerado para demonstrar relatório gráfico";
+} else {
+  # Lendo arquivo json e separando os dados
+  $file = new SplFileObject($fileJson, 'r');
+  $file->seek(PHP_INT_MAX);
+  $last_line = $file->key();
+
+  if ($last_line < $limitPrint) {
+      $limitPrint = $last_line;
+  }
+
+  $lines = new LimitIterator($file, $last_line - $limitPrint, $last_line);
+  $array = iterator_to_array($lines);
+
+  if ($limitPrint >= 30) {
+      for ($i = $last_line; $i > $last_line - $limitPrint; $i -= 2) {
+          unset($array[$i]);
+      }
+  }
+
+  # Variáveis de retorno no arquivo json
+  $temperatura_termometro = array();
+  $temperatura_setado = array();
+  $limite_temperatura_alta = array();
+  $limite_temperatura_baixa = array();
+  $status_do_freezer = array();
+  $tempo_freezer_status = array();
+  $data = array();
+
+  $countON = 0;
+  $countOFF = 0;
+
+  foreach ($array as $key => $value) {
+      if (!empty($value)) {
+          $output = str_replace("'",'"',$value);
+          $output = utf8_encode($output);
+          $json = json_decode($output);
+
+          array_push($temperatura_termometro, $json->{'temperatura termometro'});
+          array_push($temperatura_setado, $json->{'temperatura setado'});
+          array_push($limite_temperatura_alta, $json->{'limite temperatura alta'});
+          array_push($limite_temperatura_baixa, $json->{'limite temperatura baixa'});
+          array_push($status_do_freezer, $json->{'status do freezer'});
+          array_push($tempo_freezer_status, $json->{'tempo freezer status'});
+          array_push($data, $json->{'data'});
+
+          if ($json->{'status do freezer'} == 1) {
+            $countON++;
+          } elseif ($json->{'status do freezer'} == 0) {
+            $countOFF++;
+          }
+      }
+  }
+
+  $result_temperatura_termometro = json_encode(array_values($temperatura_termometro));
+  $result_temperatura_setado =json_encode(array_values($temperatura_setado));
+  $result_limite_temperatura_alta = json_encode(array_values($limite_temperatura_alta));
+  $result_limite_temperatura_baixa = json_encode(array_values($limite_temperatura_baixa));
+  $result_status_do_freezer = array_values($status_do_freezer);
+  $result_tempo_freezer_status = array_values($tempo_freezer_status);
+  $result_data = json_encode(array_values($data));
 }
-
-# Variáveis de retorno no arquivo json
-$temperatura_termometro = array();
-$temperatura_setado = array();
-$limite_temperatura_alta = array();
-$limite_temperatura_baixa = array();
-$status_do_freezer = array();
-$tempo_freezer_status = array();
-$data = array();
-
-# Lendo arquivo json e separando os dados
-$file = new SplFileObject($fileJson, 'r');
-$file->seek(PHP_INT_MAX);
-$last_line = $file->key();
-
-if ($last_line < $limitPrint) {
-    $limitPrint = $last_line;
-}
-
-$lines = new LimitIterator($file, $last_line - $limitPrint, $last_line);
-$array = iterator_to_array($lines);
-
-if ($limitPrint >= 30) {
-    for ($i = $last_line; $i > $last_line - $limitPrint; $i -= 2) {
-        unset($array[$i]);
-    }
-}
-
-foreach ($array as $key => $value) {
-    if (!empty($value)) {
-        $output = str_replace("'",'"',$value);
-        $output = utf8_encode($output);
-        $json = json_decode($output);
-
-        array_push($temperatura_termometro, $json->{'temperatura termometro'});
-        array_push($temperatura_setado, $json->{'temperatura setado'});
-        array_push($limite_temperatura_alta, $json->{'limite temperatura alta'});
-        array_push($limite_temperatura_baixa, $json->{'limite temperatura baixa'});
-        array_push($status_do_freezer, $json->{'status do freezer'});
-        array_push($tempo_freezer_status, $json->{'tempo freezer status'});
-        array_push($data, $json->{'data'});
-    }
-}
-
-$result_temperatura_termometro = json_encode(array_values($temperatura_termometro));
-$result_temperatura_setado =json_encode(array_values($temperatura_setado));
-$result_limite_temperatura_alta = json_encode(array_values($limite_temperatura_alta));
-$result_limite_temperatura_baixa = json_encode(array_values($limite_temperatura_baixa));
-$result_status_do_freezer = array_values($status_do_freezer);
-$result_tempo_freezer_status = array_values($tempo_freezer_status);
-$result_data = json_encode(array_values($data));
 ?>
 
 <!DOCTYPE html>
@@ -72,9 +81,6 @@ $result_data = json_encode(array_values($data));
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Thema Bootstrap -->
-    <link rel="stylesheet" type="text/css" href="https://bootswatch.com/yeti/bootstrap.min.css">
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -117,12 +123,21 @@ $result_data = json_encode(array_values($data));
         </div>
 
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+          <?php if ($alert == null) : ?>
+
           <div class="panel panel-default">
             <div class="panel-heading text-center">Gráfico de temperaturas</div>
             <div class="panel-body">
               <canvas id="chartTemperatura"></canvas>
             </div>
           </div>
+
+          <?php else : ?>
+
+          <div class="alert alert-danger text-center" role="alert"><i class="fa fa-exclamation-circle" aria-hidden="true"></i> <?="$alert"?></div>
+
+          <?php endif ?>
         </div>
         <!-- ! CONTENT -->
 
